f236786a424e624c26aff66381d7ab66b166dadc
Axel Jimenez <axely7@hotmail.com>
2023-02-01 11:16:26 -0600

    showing ticket in display and init with desktop

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,44 @@
+// Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button')
 
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search)
+
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio')
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio
+
+
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log( payload )
+})
+
+socket.on("ultimo-ticket", (ultimoTicket) => {
+//    lblNuevoTicket.innerHTML = 'Ticket: ' + ultimoTicket
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+
+    // socket.emit( 'siguient-ticket', null, ( ticket ) => {
+    //    lblNuevoTicket.innerHTML = ticket
+    // });
+
+});
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,34 @@
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button')
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnCrear.disabled = false;
+});
+
+socket.on('disconnect', () => {
+   btnCrear.disabled = true;
+});
+
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log( payload )
+})
+
+socket.on("ultimo-ticket", (ultimoTicket) => {
+   lblNuevoTicket.innerHTML = 'Ticket: ' + ultimoTicket
+})
+
+
+btnCrear.addEventListener( 'click', () => {
+
+    socket.emit( 'siguient-ticket', null, ( ticket ) => {
+       lblNuevoTicket.innerHTML = ticket
+    });
+
+});
